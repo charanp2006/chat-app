1da5b7a1169e494e898b6eeb34b31fd057fac009
Charan P <charan.p.8653@gmail.com>
2025-08-08 19:55:57 +0530

    first commit

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import Sidebar from '../components/sidebar'
+import Sidebar from '../components/Sidebar.jsx'
 import ChatContainer from '../components/ChatContainer'
 import RightSidebar from '../components/RightSidebar'
 import { ChatContext } from '../../context/ChatContext'
